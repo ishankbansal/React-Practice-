ca3c980c34d8c4ff95b1397ec5bcb4b66c343274
Ishank Bansal <ishankbansal1111@gmail.com>
2022-04-12 22:47:36 +0530

    Components

---
+++ Header.js
@@ -0,0 +1,14 @@
+export default function Header()
+{
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-ReactDOM.render(<h1>Hello, Everyone</h1>, document.getElementById("root"))
+function Header()
+{
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Page(){
+    return (
+        <div>
+            <Header/>
+            <ol>
+                <li>It's a popular library, so I'll be able to fit in with the cool kids</li>
+                <li>I'am more likely to get a job as a developer if I know React</li>
+            </ol>
+        </div>
+        
+    )
+}
+ReactDOM.render(<Page/>, document.getElementById("root"))
